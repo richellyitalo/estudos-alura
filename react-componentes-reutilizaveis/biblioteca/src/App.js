f0ef0dad91a4aa146992fa4026842d05181b8a6a
Richelly Italo <Richelly Italo>
2017-03-24 16:13:45 -0300

    React: Componentes criados.

--- react-componentes-reutilizaveis/biblioteca/src/App.js
+++ react-componentes-reutilizaveis/biblioteca/src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {lista: []};
+        this.state = {lista: [], nome: '', email: '', senha: ''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
     }
 
     componentWillMount() {
@@ -19,6 +25,37 @@ class App extends Component {
                 })
             }.bind(this)
         });
+    }
+
+    enviaForm(event) {
+        event.preventDefault();
+        $.ajax({
+            url: 'http://localhost:8080/api/autores',
+            contentType: 'application/json',
+            dataType: 'json',
+            type: 'post',
+            data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
+            success: function(response) {
+                this.setState({lista: response});
+                this.setState({nome: '', email: '', senha: ''});
+            }
+            .bind(this),
+            error: function(response) {
+                console.log('erro', response);
+            }
+        });
+    }
+
+    setNome(event) {
+        this.setState({nome: event.target.value});
+    }
+
+    setEmail(event) {
+        this.setState({email: event.target.value});
+    }
+
+    setSenha(event) {
+        this.setState({senha: event.target.value});
     }
 
     render() {
@@ -46,23 +83,11 @@ class App extends Component {
                     </div>
                     <div className="content" id="content">
                         <div className="pure-form pure-form-aligned">
-                            <form className="pure-form pure-form-aligned">
-                                <div className="pure-control-group">
-                                    <label htmlFor="nome">Nome</label>
-                                    <input id="nome" type="text" name="nome" value=""  />
-                                </div>
-                                <div className="pure-control-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input id="email" type="email" name="email" value=""  />
-                                </div>
-                                <div className="pure-control-group">
-                                    <label htmlFor="senha">Senha</label>
-                                    <input id="senha" type="password" name="senha"  />
-                                </div>
-                                <div className="pure-control-group">
-                                    <label></label>
-                                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                                </div>
+                            <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
+                                <InputCustomizado name="nome" type="text" onChange={this.setNome} value={this.state.nome} label="Nome"/>
+                                <InputCustomizado name="email" type="email" onChange={this.setEmail} value={this.state.email} label="E-mail"/>
+                                <InputCustomizado name="senha" type="text" onChange={this.setSenha} value={this.state.senha} label="Senha"/>
+                                <BotaoSubmitCustomizado label="Gravar"/>
                             </form>
 
                         </div>
@@ -70,6 +95,7 @@ class App extends Component {
                             <table className="pure-table">
                                 <thead>
                                 <tr>
+                                    <th>id</th>
                                     <th>Nome</th>
                                     <th>email</th>
                                 </tr>
@@ -78,7 +104,8 @@ class App extends Component {
                                 {
                                     this.state.lista.map(function(item) {
                                         return (
-                                          <tr>
+                                          <tr key={item.id.toString()}>
+                                              <td>{item.id}</td>
                                               <td>{item.nome}</td>
                                               <td>{item.email}</td>
                                           </tr>
