545d42d81c318de347e865894e1e7944a1ed6720
Richelly Italo <richellyitalo@gmail.com>
2017-07-05 22:01:09 -0300

    Components

--- angular2-webapps/alurapic/client/app/app.component.ts
+++ angular2-webapps/alurapic/client/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
+    moduleId: module.id,
     selector: 'app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+    fotos : Array<Object> = []; // ou fotos : Object[] = [];
     
+    constructor(http: Http)
+    {
+        http
+        .get('v1/fotos')
+        .map(res => res.json())
+        .subscribe(fotos => {
+            this.fotos = fotos;
+        });
+    }
 }
