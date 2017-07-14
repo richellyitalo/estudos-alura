6a05e07e0c3188b69dcf4e1757507d2ae0542bfa
Richelly Italo <richellyitalo@gmail.com>
2017-07-14 13:01:26 -0300

    HTML inicial

--- react2-container-component/instalura/src/index.js
+++ react2-container-component/instalura/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './css/reset.css';
+import './css/timeline.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
